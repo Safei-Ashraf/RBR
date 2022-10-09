4beddb60c5f76accb3f79d51b41abf1900f123c9
Safei Ashraf <safei.ashraf@gmail.com>
2022-10-09 20:39:13 +0200

    feat: create todo basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
+interface TodoItem {
+	text: string;
+	id: string;
+}
+function makeid(length: number): string {
+	var result = "";
+	var characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(
+			Math.floor(Math.random() * charactersLength)
+		);
+	}
+	return result;
+}
 function App() {
-  const [count, setCount] = useState(0)
+	const [todo, setTodo] = useState<TodoItem>({ text: "", id: "" });
+	const [todoItems, setTodoItems] = useState<TodoItem[]>([todo]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTodo({ text: e.target.value, id: makeid(12) });
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (todo.text.length > 0) {
+			setTodoItems((todoItems) => [...(todoItems || []), todo]);
+			setTodo({ text: "", id: "" });
+		}
+	};
+
+	return (
+		<div className="App">
+			<h1>Fancy💥App</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Bring laundary.."
+					value={todo.text}
+					className="todo-input"
+					onChange={handleChange}
+				/>
+				<button type="submit">
+					Add <span>TODO</span>
+				</button>
+			</form>
+			<div className="card">
+				{todoItems?.length > 0 && (
+					<div className="card">
+						{todoItems.map((todo) => (
+							<p key={todo.id}>{todo.text}</p>
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
